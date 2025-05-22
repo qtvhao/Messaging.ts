a9950cf40c54f81395e03e095aca6e8627d4bd7b
Hao Nghiem <qtvhao@gmail.com>
2025-05-22 22:44:26 +0700

    Add

--- src/Infrastructure/Messaging/EventBusServiceProvider.ts
+++ src/Infrastructure/Messaging/EventBusServiceProvider.ts
@@ -12,14 +12,20 @@ import {
   TopicRegistry,
 } from "support.ts";
 import { EventBus } from "./EventBus";
+import { MessageBrokerFactory } from "../BrokerFactory/MessageBrokerFactory";
 
 export class EventBusServiceProvider extends ServiceProvider
   implements IServiceProvider {
   private eventBus!: IEventBus;
 
   register(): void {
-    this.app.bind<TopicRegistry>(TopicRegistry).toConstantValue(new TopicRegistry);
-    this.app.bind<IEventMapperRegistry>(TYPES.EventMapperRegistry).toConstantValue(new EventMapperRegistry);
+    this.app.bind<IMessageBrokerFactory>(TYPES.MessageBrokerFactory)
+      .toConstantValue(new MessageBrokerFactory());
+    this.app.bind<TopicRegistry>(TopicRegistry).toConstantValue(
+      new TopicRegistry(),
+    );
+    this.app.bind<IEventMapperRegistry>(TYPES.EventMapperRegistry)
+      .toConstantValue(new EventMapperRegistry());
     const driver: BrokerType = process.env.EVENT_BUS_DRIVER || "inmemory";
 
     const topicRegistry = this.app.get<TopicRegistry>(TopicRegistry);
