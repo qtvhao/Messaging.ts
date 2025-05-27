01e911f37c0b67edbb2d7c081b15ae1120f0169c
Hao Nghiem <qtvhao@gmail.com>
2025-05-27 22:58:00 +0700

    Add

--- src/Infrastructure/Messaging/EventBusFactory.ts
+++ src/Infrastructure/Messaging/EventBusFactory.ts
@@ -1,18 +1,43 @@
-import { IConfigurationService, IDomainEvent, IDomainEventMapperRegistry, IEventBus, IEventHandlerResolver, IEventTopicMapper, IMessageBrokerFactory, Message } from "contracts.ts";
+import {
+  IConfigurationService,
+  IDomainEvent,
+  IDomainEventMapperRegistry,
+  IEventBus,
+  IEventHandlerResolver,
+  IEventTopicMapper,
+  IMessageBrokerFactory,
+  Message,
+  TYPES,
+} from "contracts.ts";
 import { EventBus } from "./EventBus";
+import { inject } from "inversify";
 
 export class EventBusFactory {
   constructor(
-    private readonly brokerFactory: IMessageBrokerFactory,
-    private readonly topicMapper: IEventTopicMapper,
-    private readonly eventMapperRegistry: IDomainEventMapperRegistry<IDomainEvent, Message>,
-    private readonly configService: IConfigurationService,
-    private readonly handlerResolver: IEventHandlerResolver
+    @inject(TYPES.MessageBrokerFactory) private readonly brokerFactory:
+      IMessageBrokerFactory,
+    @inject(TYPES.EventTopicMapper) private readonly topicMapper:
+      IEventTopicMapper,
+    @inject(
+      TYPES.DomainEventMapperRegistry,
+    ) private readonly eventMapperRegistry: IDomainEventMapperRegistry<
+      IDomainEvent,
+      Message
+    >,
+    @inject(TYPES.ConfigurationService) private readonly configService:
+      IConfigurationService,
+    @inject(TYPES.EventHandlerResolver) private readonly handlerResolver:
+      IEventHandlerResolver,
   ) {}
 
   create(): IEventBus {
     const driver = this.configService.getEventBusDriver();
     const broker = this.brokerFactory.create(driver);
-    return new EventBus(broker, this.topicMapper, this.eventMapperRegistry, this.handlerResolver);
+    return new EventBus(
+      broker,
+      this.topicMapper,
+      this.eventMapperRegistry,
+      this.handlerResolver,
+    );
   }
 }
